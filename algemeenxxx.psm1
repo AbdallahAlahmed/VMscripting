a3cf5f50fc037e59922c292637ff1aad5301008d
AbdallahAlahmed <101391585+AbdallahAlahmed@users.noreply.github.com>
2024-05-07 11:42:45 +0200

    Set-Basisconfiguratie  function done

--- algemeenxxx.psm1
+++ algemeenxxx.psm1
@@ -5,17 +5,53 @@
 .SYNOPSIS
 Voert de basisconfiguratie van een Windows apparaat uit.
 #>
-function Do-Basisconfiguratie {
+function Set-Basisconfiguratie {
     # Plaats hier de code voor de basisconfiguratie van een Windows device
     Write-Host "Basisconfiguratie van Windows device wordt uitgevoerd..."
     # Voorbeeld: Installeer software, configureer netwerkinstellingen, etc.
-    
+    # Pad naar het XML-bestand
+    $XmlFilePath = "Z:\scripting\settings\instellingen.xml"
+
+    # Laden van XML
+    $xml = [xml](Get-Content $XmlFilePath)
+
+    # Functie om de parameters aan te passen
+    function Update-XMLParameters {
+        param(
+            [string]$ServerInitialen,
+            [string[]]$NieuweMACAdressen
+        )
+        # Aanpassen van servernaam
+        $xml.Configuration.ServerName = $xml.Configuration.ServerName + $ServerInitialen
+
+        # Aanpassen van MAC-adressen
+        for ($i = 0; $i -lt $xml.Configuration.NetworkAdapters.NetworkAdapter.Count; $i++) {
+            $xml.Configuration.NetworkAdapters.NetworkAdapter[$i].MACAddress = $NieuweMACAdressen[$i]
+        }
+
+        # Opslaan van wijzigingen naar XML-bestand
+        $xml.Save($XmlFilePath)
+    }
+
+    # Vraag de gebruiker om initialen en nieuwe MAC-adressen
+    $ServerInitialen = Read-Host "Voer uw initialen in voor de servernaam:"
+    $NieuweMACAdressen = @()
+    for ($i = 1; $i -le $xml.Configuration.NetworkAdapters.NetworkAdapter.Count; $i++) {
+        $NieuwMAC = Read-Host "Voer het nieuwe MAC-adres in voor netwerkadapter $i" 
+        $NieuweMACAdressen += $NieuwMAC
+    }
+
+    # Update XML-parameters
+    Update-XMLParameters -ServerInitialen $ServerInitialen -NieuweMACAdressen $NieuweMACAdressen
+
+    Write-Host "Parameters succesvol bijgewerkt in $XmlFilePath."
+
     # Log de wijziging
-    Log-Change "Basisconfiguratie van Windows device uitgevoerd."
+    Write-Log "Basisconfiguratie van Windows device uitgevoerd."
 }
 
 # Functie om wijzigingen te loggen
-function Log-Change {
+function Write-Log {
     param(
         [string]$Change
     )
@@ -24,4 +60,4 @@ function Log-Change {
     Add-Content -Path $LogFilePath -Value "$Timestamp - $Change"
 }
 
-Export-ModuleMember Do-Basisconfiguratie -FunctionDefinition
+Export-ModuleMember Set-Basisconfiguratie -FunctionDefinition
